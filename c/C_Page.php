b8a056b330d5b92a76adba4b06160de9918b84fc
MironovVS <MAdd2062@gmail.com>
2015-12-05 16:27:04 +0400

    изменили название класса

--- c/C_Page.php
+++
@@ -1,1 +0,0 @@
-<?php//// Конттроллер страницы чтения.//require_once('m/model.php');class C_Page extends C_Base{	public function action_index(){		$this->title .= '::Чтение';		$text = text_get();		$this->content = $this->Template('v/v_index.php', array('text' => $text));		}	}
